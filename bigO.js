d5793db7494485812f7e3656b47b3883eea139d8
Nghi Tran <nghi.t.tran8@gmail.com>
2020-03-03 08:57:17 -0800

    updates drills

--- bigO.js
+++ bigO.js
@@ -2,5 +2,14 @@
 // 1a) O(1) - Regardless of how many dog owners there are in the room, we yell once to inquire and we get 1 reply
 // 1b) O(n) - We have to ask each dog owner until we find a matching breed dog owner. Worst case is we have to ask every single dog owner.
 // 2. Even or odd - O(1) Even or odd, 1 boolean value has to return.
-//3. Are you here? - O(n^2) Nested for loops, which means that the outer array will run thorugh the inner array for every element in the first array.
-//4. Doubler - O(n) Reads value of array only once each.
+// 3. Are you here? - O(n^2) Nested for loops, which means that the outer array will run thorugh the inner array for every element in the first array.
+// 4. Doubler - O(n) Reads value of array only once each.
+// 5. Naive Search - O(n) Reads once for each index in array to find the item
+// 6. Creating pairs - O(n^2) Nested for loops, which means that the index of the outer loop will run through every index of the inner loop.
+// 7. Compute the sequence - O()
+// 8. An efficient search - O()
+// 9. Random element - O()
+// 10. What Am I? - O()
+// 11. Tower of Hanoi - O()
+// 12. Iterative version - O()
+// 13. Recursive Big O - O()
