e055cdba880510cedb75f2cf053d3e6b9e09debf
Nghi Tran <nghi.t.tran8@gmail.com>
2020-03-03 09:48:02 -0800

    update drills

--- bigO.js
+++ bigO.js
@@ -6,10 +6,33 @@
 // 4. Doubler - O(n) Reads value of array only once each.
 // 5. Naive Search - O(n) Reads once for each index in array to find the item
 // 6. Creating pairs - O(n^2) Nested for loops, which means that the index of the outer loop will run through every index of the inner loop.
-// 7. Compute the sequence - O(n) cause no matter how big of a numbner input, the program has to itterate once for every preceding number and excute only one statment. 
+// 7. Compute the sequence - O(n) cause no matter how big of a numbner input, the program has to itterate once for every preceding number and excute only one statment.
 // 8. An efficient search - O(log(n)) regardless of the input size of the array we're always cutting the array into halves to find the item.
-// 9. Random element - O()
-// 10. What Am I? - O()
+// 9. Random element - O(1) Regardless of the input size of the array, the program will always return 1 random number from the array.
+// 10. What Am I? - O(n)
 // 11. Tower of Hanoi - O()
+
+// Input/Output
+// (3) => moved disk 1 to Rod C
+// => moved disk 2 to Rod B
+// => moved disk 1 to Rod B
+// => moved disk 3 to Rod C
+// => moved disk 1 to Rod A
+// => moved disk 2 to Rod C
+// => moved disk 1 to Rod C
+
+// Algorithm
+// Initial position: 0,0
+// Always check for location of disk 1
+// Checking if Rod C is empty, if so move the smallest disk there
+
+// function TowerofHanoi(num) {
+//   let board = [[],
+//   [],
+// ];
+//   // base
+//   if ()
+// }
+
 // 12. Iterative version - O()
 // 13. Recursive Big O - O()
