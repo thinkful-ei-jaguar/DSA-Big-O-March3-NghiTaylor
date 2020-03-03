11e471af9b0d479cac72fa8e63341713658bcdd9
Nghi Tran <nghi.t.tran8@gmail.com>
2020-03-03 08:41:33 -0800

    first 2 drills

---
+++ bigO.js
@@ -0,0 +1,4 @@
+// 1. What is the Big O for this?
+// 1a) O(1) - Regardless of how many dog owners there are in the room, we yell once to inquire and we get 1 reply
+// 1b) O(n) - We have to ask each dog owner until we find a matching breed dog owner. Worst case is we have to ask every single dog owner.
+// 2. Even or odd - O(1) Even or odd, 1 boolean value has to return.
